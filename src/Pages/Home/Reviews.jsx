1911ecf264ab351dabfbfe2bc3ef884b4a6ea050
Austin O <guitarguy199@gmail.com>
2023-09-02 15:34:38 -0600

    style improvements

--- src/Pages/Home/Reviews.jsx
+++ src/Pages/Home/Reviews.jsx
@@ -1,7 +1,9 @@
 import ReviewCard from './ReviewCard';
 import './Reviews.css';
 import christinaImg from './assets/christina-avatar.jpeg';
-import andreaImg from './assets/andrea-avatar.jpeg';
+import andreaImg from './assets/andrea-avatar.png';
+import johnImg from './assets/john.jpeg';
+import tonyaImg from './assets/tonya.jpeg';
 
 const Reviews = () => {
 
@@ -10,20 +12,25 @@ const Reviews = () => {
             name: 'Andrea',
             image: andreaImg, 
             rating: '⭐️⭐️⭐️⭐️⭐️',
-            review: "Amazing!"
+            review: "The dedication to the quality and presentation really shows in every bite. I'll be back!"
         },
         {
             name: 'Christina',
             image: christinaImg,
             rating: '⭐️⭐️⭐️⭐️⭐️',
+            review: 'The smoked feta gyro wrap was to die for!'
         },
         {
-            name: 'James',
-            rating: '⭐️⭐️⭐️⭐️⭐️',
+            name: 'John',
+            image: johnImg,
+            rating: '⭐️⭐️⭐️⭐️',
+            review: 'Best Mediterranean food on this side of the pond.'
         },
         {
-            name: 'James',
+            name: 'Tonya',
+            image: tonyaImg,
             rating: '⭐️⭐️⭐️⭐️⭐️',
+            review: 'You HAVE to try the garlic toum with pita. Unreal.'
         },
     ]
 
